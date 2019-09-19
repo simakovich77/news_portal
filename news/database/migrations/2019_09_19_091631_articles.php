1554b93eedfa60adb4ab6f25fbc500ce70becece
Ivan Simakovich <developer@ctech>
2019-09-19 12:49:01 +0300

    database_schema

---
+++ news/database/migrations/2019_09_19_091631_articles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id_article');
+            $table->text('title_article');
+            $table->text('body_article');
+            $table->text('picture_article');
+            $table->text('category_article');
+            $table->text('tag_article');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
